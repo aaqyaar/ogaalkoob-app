23ef25cb454d1df6fab7216a06d0c16fe5d5482e
abdizamedmo <abdizamedmo@gmail.com>
2023-11-23 11:41:51 +0300

    Added <Icon/> in SVG REGISTRY

--- app/components/Icon.tsx
+++ app/components/Icon.tsx
@@ -10,14 +10,21 @@ import {
   ViewProps,
   ViewStyle,
 } from "react-native"
+import { Svg, Path } from "react-native-svg"
 
 export type IconTypes = keyof typeof iconRegistry
+export type SvgIconTypes = keyof ReturnType<typeof svgRegistry>
 
 interface IconProps extends TouchableOpacityProps {
   /**
    * The name of the icon
    */
-  icon: IconTypes
+  icon?: IconTypes
+
+  /**
+   * The name of the svg icon
+   */
+  svgIcon?: SvgIconTypes
 
   /**
    * An optional tint color for the icon
@@ -54,6 +61,7 @@ interface IconProps extends TouchableOpacityProps {
 export function Icon(props: IconProps) {
   const {
     icon,
+    svgIcon,
     color,
     size,
     style: $imageStyleOverride,
@@ -79,7 +87,11 @@ export function Icon(props: IconProps) {
       {...WrapperProps}
       style={$containerStyleOverride}
     >
-      <Image style={$imageStyle} source={iconRegistry[icon]} />
+      {icon ? (
+        <Image style={$imageStyle} source={iconRegistry[icon]} />
+      ) : (
+        svgRegistry(size, color)[svgIcon as SvgIconTypes]
+      )}
     </Wrapper>
   )
 }
@@ -107,6 +119,97 @@ export const iconRegistry = {
   slack: require("../../assets/icons/demo/slack.png"),
   view: require("../../assets/icons/view.png"),
   x: require("../../assets/icons/x.png"),
+}
+
+export const svgRegistry = (size = 24, color?: string) => {
+  return {
+    search: (
+      <Svg
+        // xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke={color || "currentColor"}
+        width={size}
+        height={size}
+      >
+        <Path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+        />
+      </Svg>
+    ),
+    home: (
+      <Svg
+        // xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke={color || "currentColor"}
+        width={size}
+        height={size}
+      >
+        <Path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+        />
+      </Svg>
+    ),
+    cart: (
+      <Svg
+        // xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke={color || "currentColor"}
+        width={size}
+        height={size}
+      >
+        <Path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+        />
+      </Svg>
+    ),
+
+    purchases: (
+      <Svg
+        // xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke={color || "currentColor"}
+        width={size}
+        height={size}
+      >
+        <Path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z"
+        />
+      </Svg>
+    ),
+    profile: (
+      <Svg
+        // xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke={color || "currentColor"}
+        width={size}
+        height={size}
+      >
+        <Path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+        />
+      </Svg>
+    ),
+  }
 }
 
 const $imageStyleBase: ImageStyle = {
