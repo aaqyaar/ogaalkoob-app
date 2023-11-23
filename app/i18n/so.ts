92cf780445954e581556473c2118c3adba65f91f
abdizamedmo <abdizamedmo@gmail.com>
2023-11-23 11:01:31 +0300

    Languages for adding the new screens

---
+++ app/i18n/so.ts
@@ -0,0 +1,65 @@
+import { Translations } from "./en"
+
+const so: Translations = {
+  common: {
+    ok: "",
+    cancel: "",
+    back: "",
+    logOut: "",
+  },
+  welcomeScreen: {
+    postscript: "",
+    readyForLaunch: "",
+    exciting: "",
+    letsGo: "",
+  },
+  errorScreen: {
+    title: "",
+    friendlySubtitle: "",
+    reset: "",
+    traceTitle: "",
+  },
+  emptyStateComponent: {
+    generic: {
+      heading: "",
+      content: "",
+      button: "",
+    },
+  },
+  errors: {
+    invalidEmail: "",
+  },
+  loginScreen: {
+    signIn: "",
+    enterDetails: "",
+    emailFieldLabel: "",
+    passwordFieldLabel: "",
+    emailFieldPlaceholder: "",
+    passwordFieldPlaceholder: "",
+    tapToSignIn: "",
+  },
+  forgotPasswordScreen: {
+    forgotPassword: "",
+    enterDetails: "",
+    emailFieldLabel: "",
+    emailFieldPlaceholder: "",
+    tapToReset: "",
+  },
+  verifyCodeScreen: {
+    verifyCode: "",
+    enterDetails: "",
+    codeFieldLabel: "",
+    codeFieldPlaceholder: "",
+    tapToVerify: "",
+  },
+  resetPasswordScreen: {
+    resetPassword: "",
+    enterDetails: "",
+    passwordFieldLabel: "",
+    confirmPasswordFieldLabel: "",
+    passwordFieldPlaceholder: "",
+    tapToReset: "",
+  },
+}
+
+export default so
