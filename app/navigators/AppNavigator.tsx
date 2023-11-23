b6b0f6137c094e470e672944599753302606c873
abdizamedmo <abdizamedmo@gmail.com>
2023-11-23 11:53:34 +0300

    BottomTab navigator in authenticated screens

--- app/navigators/AppNavigator.tsx
+++ app/navigators/AppNavigator.tsx
@@ -11,15 +11,15 @@ import {
   NavigatorScreenParams,
 } from "@react-navigation/native"
 import { createNativeStackNavigator, NativeStackScreenProps } from "@react-navigation/native-stack"
-import { observer } from "mobx-react-lite"
 import React from "react"
 import { useColorScheme } from "react-native"
 import * as Screens from "app/screens"
 import Config from "../config"
-import { useStores } from "../models"
-import { DemoNavigator, DemoTabParamList } from "./DemoNavigator"
 import { navigationRef, useBackButtonHandler } from "./navigationUtilities"
 import { colors } from "app/theme"
+import { useAuthStore } from "app/models"
+import { translate } from "app/i18n"
+import { TabNavigator, TabNavigatorParamList } from "./TabNavigator"
 
 /**
  * This type allows TypeScript to know what routes are defined in this navigator
@@ -35,11 +35,13 @@ import { colors } from "app/theme"
  *   https://reactnavigation.org/docs/typescript/#organizing-types
  */
 export type AppStackParamList = {
-  Welcome: undefined
   Login: undefined
-  Demo: NavigatorScreenParams<DemoTabParamList>
+  App: NavigatorScreenParams<TabNavigatorParamList>
   // 🔥 Your screens go here
-  // IGNITE_GENERATOR_ANCHOR_APP_STACK_PARAM_LIST
+  ForgotPassword: undefined
+  VerifyCode: { email: string }
+  ResetPassword: { userId: string }
+  Register: undefined
 }
 
 /**
@@ -56,38 +58,100 @@ export type AppStackScreenProps<T extends keyof AppStackParamList> = NativeStack
 // Documentation: https://reactnavigation.org/docs/stack-navigator/
 const Stack = createNativeStackNavigator<AppStackParamList>()
 
-const AppStack = observer(function AppStack() {
-  const {
-    authenticationStore: { isAuthenticated },
-  } = useStores()
+const AppStack = function AppStack() {
+  const { isAuthenticated } = useAuthStore()
 
   return (
     <Stack.Navigator
-      screenOptions={{ headerShown: false, navigationBarColor: colors.background }}
-      initialRouteName={isAuthenticated ? "Welcome" : "Login"}
+      screenOptions={{
+        headerShown: true,
+        headerBackTitle: "Back",
+        headerTintColor: colors.palette.primary500,
+        headerTitleStyle: {
+          color: colors.palette.neutral900,
+        },
+        // headerShadowVisible: false,
+        navigationBarColor: colors.background,
+      }}
+      initialRouteName={isAuthenticated ? "App" : "Login"}
     >
       {isAuthenticated ? (
         <>
-          <Stack.Screen name="Welcome" component={Screens.WelcomeScreen} />
-
-          <Stack.Screen name="Demo" component={DemoNavigator} />
+          {/* <Stack.Screen
+            name="Welcome"
+            component={Screens.WelcomeScreen}
+            options={{
+              headerShown: true,
+              headerLargeTitle: true,
+              headerTitle: "Hey there!",
+              headerSearchBarOptions: {
+                placeholder: "Search",
+              },
+            }}
+          /> */}
+          <Stack.Screen
+            name="App"
+            component={TabNavigator}
+            options={{
+              headerShown: false,
+            }}
+          />
         </>
       ) : (
         <>
-          <Stack.Screen name="Login" component={Screens.LoginScreen} />
+          <Stack.Screen
+            name="Login"
+            options={{
+              headerTitle: translate("loginScreen.signIn"),
+            }}
+            component={Screens.LoginScreen}
+          />
+
+          <Stack.Screen
+            name="Register"
+            options={{
+              headerTitle: translate("registerScreen.register"),
+            }}
+            component={Screens.RegisterScreen}
+          />
+
+          <Stack.Screen
+            name="ForgotPassword"
+            options={{
+              headerTitle: translate("forgotPasswordScreen.forgotPassword"),
+            }}
+            component={Screens.ForgotPasswordScreen}
+          />
+          <Stack.Screen
+            name="VerifyCode"
+            options={{
+              headerTitle: translate("verifyCodeScreen.verifyCode"),
+            }}
+            initialParams={{
+              email: "",
+            }}
+            component={Screens.VerifyCodeScreen}
+          />
+          <Stack.Screen
+            name="ResetPassword"
+            options={{
+              headerTitle: translate("resetPasswordScreen.resetPassword"),
+            }}
+            initialParams={{
+              userId: "",
+            }}
+            component={Screens.ResetPasswordScreen}
+          />
         </>
       )}
-
-      {/** 🔥 Your screens go here */}
-      {/* IGNITE_GENERATOR_ANCHOR_APP_STACK_SCREENS */}
     </Stack.Navigator>
   )
-})
+}
 
 export interface NavigationProps
   extends Partial<React.ComponentProps<typeof NavigationContainer>> {}
 
-export const AppNavigator = observer(function AppNavigator(props: NavigationProps) {
+export const AppNavigator = function AppNavigator(props: NavigationProps) {
   const colorScheme = useColorScheme()
 
   useBackButtonHandler((routeName) => exitRoutes.includes(routeName))
@@ -101,4 +165,4 @@ export const AppNavigator = observer(function AppNavigator(props: NavigationProp
       <AppStack />
     </NavigationContainer>
   )
-})
+}
---
+++ app/navigators/TabNavigator.tsx
@@ -0,0 +1,132 @@
+import React from "react"
+import * as Screens from "app/screens"
+import { BottomTabScreenProps, createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { colors, typography } from "app/theme"
+import { Platform, TextStyle, ViewStyle } from "react-native"
+import { Icon } from "app/components"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+import { CompositeScreenProps } from "@react-navigation/native"
+import { AppStackParamList, AppStackScreenProps } from "./AppNavigator"
+
+export type TabNavigatorParamList = {
+  Home: undefined
+  Purchases: undefined
+  Profile: undefined
+  Cart: undefined
+}
+
+const Tab = createBottomTabNavigator<TabNavigatorParamList>()
+
+export type TabScreenProps<T extends keyof TabNavigatorParamList> = CompositeScreenProps<
+  BottomTabScreenProps<TabNavigatorParamList, T>,
+  AppStackScreenProps<keyof AppStackParamList>
+>
+
+export const TabNavigator = () => {
+  const { bottom } = useSafeAreaInsets()
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        // headerShown: false,
+        tabBarHideOnKeyboard: true,
+        tabBarStyle: [
+          $tabBar,
+          {
+            height: bottom + 55,
+            marginBottom: Platform.OS === "ios" ? 0 : 5,
+            paddingBottom: Platform.OS === "android" ? 20 : 30,
+            paddingTop: Platform.OS === "android" ? 10 : 5,
+
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 12,
+            },
+            shadowOpacity: 0.58,
+            shadowRadius: 8.0,
+            elevation: 15,
+          },
+        ],
+        tabBarActiveTintColor: colors.text,
+        tabBarInactiveTintColor: colors.text,
+        tabBarLabelStyle: $tabBarLabel,
+        tabBarItemStyle: $tabBarItem,
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Screens.HomeScreen}
+        options={{
+          headerTitleAlign: "center",
+          headerTitle: "Ogaalkoob",
+
+          tabBarLabel: "Home",
+          headerTitleStyle: { fontFamily: typography.primary.semiBold },
+          tabBarLabelStyle: { fontFamily: typography.primary.normal },
+          tabBarIcon: ({ color, focused }) => (
+            <Icon svgIcon="home" color={focused ? colors.palette.primary500 : color} size={27} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        options={{
+          headerTitleAlign: "center",
+          headerTitle: "Ogaalkoob",
+          headerTitleStyle: { fontFamily: typography.primary.semiBold },
+          tabBarLabelStyle: { fontFamily: typography.primary.normal },
+          tabBarIcon: ({ color, focused }) => (
+            <Icon svgIcon="cart" color={focused ? colors.palette.primary500 : color} size={27} />
+          ),
+        }}
+        component={Screens.CartScreen}
+      />
+      <Tab.Screen
+        name="Purchases"
+        options={{
+          headerTitleAlign: "center",
+          headerTitle: "Ogaalkoob",
+          headerTitleStyle: { fontFamily: typography.primary.semiBold },
+          tabBarLabelStyle: { fontFamily: typography.primary.normal },
+          tabBarIcon: ({ color, focused }) => (
+            <Icon
+              svgIcon="purchases"
+              color={focused ? colors.palette.primary500 : color}
+              size={27}
+            />
+          ),
+        }}
+        component={Screens.PurchasesScreen}
+      />
+      <Tab.Screen
+        name="Profile"
+        options={{
+          headerTitleAlign: "center",
+          headerTitle: "Profile / Settings",
+          headerTitleStyle: { fontFamily: typography.primary.semiBold },
+          tabBarLabelStyle: { fontFamily: typography.primary.normal },
+          tabBarIcon: ({ color, focused }) => (
+            <Icon svgIcon="profile" color={focused ? colors.palette.primary500 : color} size={27} />
+          ),
+        }}
+        component={Screens.ProfileScreen}
+      />
+    </Tab.Navigator>
+  )
+}
+const $tabBar: ViewStyle = {
+  backgroundColor: colors.background,
+  borderTopColor: colors.transparent,
+}
+
+const $tabBarItem: ViewStyle = {
+  // paddingTop: spacing.md,
+}
+
+const $tabBarLabel: TextStyle = {
+  fontSize: 12,
+  fontFamily: typography.primary.medium,
+  lineHeight: 16,
+  flex: 1,
+}
