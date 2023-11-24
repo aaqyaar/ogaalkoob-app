c7fde6eee37d63f7c9890ba389d3692f0fffbcc0
abdizamedmo <abdizamedmo@gmail.com>
2023-11-24 12:34:23 +0300

    Configuration updated into production environments

--- app/config/config.dev.ts
+++ app/config/config.dev.ts
@@ -6,5 +6,6 @@
  * https://reactnative.dev/docs/security#storing-sensitive-info
  */
 export default {
-  API_URL: "https://oarfish-great-warthog.ngrok-free.app/api",
+  // API_URL: "https://oarfish-great-warthog.ngrok-free.app/api",
+  API_URL: "https://ogaalkoob.vercel.app/api",
 }
