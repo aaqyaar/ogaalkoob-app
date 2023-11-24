3c78d62bd92ff0ec4c77e0b3e7cd4df525725b98
abdizamedmo <abdizamedmo@gmail.com>
2023-11-24 12:37:12 +0300

    config plugins added in expo.json

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import App from "./app/app"
 import React from "react"
 import * as SplashScreen from "expo-splash-screen"
 
-SplashScreen.preventAutoHideAsync()
+// SplashScreen.preventAutoHideAsync()
 
 function IgniteApp() {
   return <App hideSplashScreen={SplashScreen.hideAsync} />
